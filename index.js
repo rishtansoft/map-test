3dd6f3a68cd0c8ef771c22ac321dcc5943286313
rishtansoft <rishtansfot@gmail.com>
2024-08-29 06:44:38 +0500

    map

---
+++ index.js
@@ -0,0 +1,16 @@
+const greet = document.getElementById("hello");
+
+const paragraphs = document.getElementsByTagName("p");
+const paragraphs1 = document.getElementsByClassName("test")
+
+let p1 = document.querySelector('p.test');
+let p = document.querySelectorAll('p.test');
+
+p[1].style.color = 'red';
+p[1].style.backgroundColor = 'blue'
+p[1].style.fontSize = '40px';
+
+
+
+
+
